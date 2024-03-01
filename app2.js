b404aeb3712a020d2206afa3dac77eb3ec02fc48
Logan Hanaway <lthanaway14@gmail.com>
2024-02-29 19:07:39 -0600

    mid vid 6

--- app2.js
+++ app2.js
@@ -74,4 +74,25 @@ const rightTriangle = {
 
 // start video 6
 // constructor functions and new
+// use uppercase onthe first letter here
+// the goal is to make a repeatable patter
+
+function Triangle(a,b){
+    this.a = a,
+    this.b = b,
+    this.getArea = function(){
+        return this.a * this.b / 2;
+    },
+    this.getHypotenuse = function() {
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
+    };
+} 
+
+// use the new operator ( IE new Set)
+const t1 = new Triangle(3,4)
+const t2 = new Triangle(9,12)
+t1.getHypotenuse() // 5
+t2.getHypotenuse() // 15
+
+// check documentation to see what new does in detail
 
