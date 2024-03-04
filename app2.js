70d8a96547f5595c4796bd726692bf0979a2152e
Logan Hanaway <lthanaway14@gmail.com>
2024-03-04 14:51:55 -0600

    start video 9

--- app2.js
+++ app2.js
@@ -24,14 +24,14 @@
 // };
 
 // one more even shorter way
-const myMath = {
-    add(x,y) {
-        return x + y;
-    },
-    square(x) {
-        return x * x;
-    } // and so on
-};
+// const myMath = {
+//     add(x,y) {
+//         return x + y;
+//     },
+//     square(x) {
+//         return x * x;
+//     } // and so on
+// };
 
 
 
@@ -39,12 +39,12 @@ const myMath = {
 // start video 5
 // this and methods
 
-function getHypotenuse(a,b) {
-    return Math.sqrt(a ** 2 + b ** 2);
-}
-function getArea(a,b) {
-    return a * b / 2;
-}
+// function getHypotenuse(a,b) {
+//     return Math.sqrt(a ** 2 + b ** 2);
+// }
+// function getArea(a,b) {
+//     return a * b / 2;
+// }
 
 // let side1 = 4;
 // let side2 = 3;
@@ -55,19 +55,19 @@ function getArea(a,b) {
 // instead of having to constantly having to reset the variables 
 // we can create a new object
 
-const rightTriangle = {
-    a: 9,
-    b: 12,
-    printThis: function() {
-        console.log(this);
-    },
-    getArea: function(){
-        return this.a * this.b / 2;
-    },
-    getHypotenuse: function() {
-        return Math.sqrt(this.a ** 2 + this.b ** 2);
-    }
-};
+// const rightTriangle = {
+//     a: 9,
+//     b: 12,
+//     printThis: function() {
+//         console.log(this);
+//     },
+//     getArea: function(){
+//         return this.a * this.b / 2;
+//     },
+//     getHypotenuse: function() {
+//         return Math.sqrt(this.a ** 2 + this.b ** 2);
+//     }
+// };
 // cannot use arrow functions here because this keyword behaves differently
 
 
@@ -77,22 +77,22 @@ const rightTriangle = {
 // use uppercase onthe first letter here
 // the goal is to make a repeatable patter
 
-function Triangle(a,b){
-    this.a = a,
-    this.b = b,
-    this.getArea = function(){
-        return this.a * this.b / 2;
-    },
-    this.getHypotenuse = function() {
-        return Math.sqrt(this.a ** 2 + this.b ** 2);
-    };
-} 
+// function Triangle(a,b){
+//     this.a = a,
+//     this.b = b,
+//     this.getArea = function(){
+//         return this.a * this.b / 2;
+//     },
+//     this.getHypotenuse = function() {
+//         return Math.sqrt(this.a ** 2 + this.b ** 2);
+//     };
+// } 
 
 // use the new operator ( IE new Set)
-const t1 = new Triangle(3,4)
-const t2 = new Triangle(9,12)
-t1.getHypotenuse() // 5
-t2.getHypotenuse() // 15
+// const t1 = new Triangle(3,4)
+// const t2 = new Triangle(9,12)
+// t1.getHypotenuse() // 5
+// t2.getHypotenuse() // 15
 
 // check documentation to see what new does in detail
 
@@ -104,17 +104,50 @@ t2.getHypotenuse() // 15
 
 // same stuff as above just different variable names in the new video
 // uses same Traingle function as above as well
-Triangle(5, 7); // RETURNS UNDEFINED!
-// USING THE NEW OPERATOR!
-const tri1 = new Triangle(3, 4);
-tri1.getHypotenuse(); // 5
-const tri2 = new Triangle(9,12);
-tri2.getHypotenuse(); // 15
+// Triangle(5, 7); // RETURNS UNDEFINED!
+// // USING THE NEW OPERATOR!
+// const tri1 = new Triangle(3, 4);
+// tri1.getHypotenuse(); // 5
+// const tri2 = new Triangle(9,12);
+// tri2.getHypotenuse(); // 15
 
 
 // skipped for now
+
+
+
+
+
 // starting video 8
+// classes intro
+
+// dont need function parans () as it is an object
+class Triangle{
+    greet(){
+        console.log("HELLO FROM TRIANGLE");
+    }
+    display(){
+        console.log(`Triangle with the sides of ${this.a} and ${this.b}`);
+    }
+}
+// method greet is included in the below triangles as it was added to it above
+// when calling we are creating a new object based off the above template
+const firstTri = new Triangle();
+firstTri.a = 3;
+firstTri.b = 4;
+const secondTri = new Triangle();
+secondTri.a = 9;
+secondTri.b = 12;
+
+firstTri.display() // in console
+secondTri.display()
+
+// we dont need to manually add a and b every time like above, we can pass them in
+// in next videi
 
 
 
+
+
+// start video 9
 
